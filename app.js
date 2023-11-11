a83de2f8b5c54ec3666758ec40581447fc797d13
Wojciech Jandura <wojtek.jandura@gmail.com>
2023-11-11 19:51:36 +0100

    Creating own middleware and 3rd-party middleware

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-//middleware
+//////////////////////////////////////////////////////
+// 1) MIDDLEWARE
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//////////////////////////////////////////////////////
+// 2) ROUTE HANDLER
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -97,6 +114,8 @@ const deleteTour = (req, res) => {
 // app.patch(`/api/v1/tours/:id`, updateTour);
 // app.delete(`/api/v1/tours/:id`, deleteTour);
 
+//////////////////////////////////////////////////////
+// 3) ROUTES
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -105,6 +124,8 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+//////////////////////////////////////////////////////
+// 4) START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
