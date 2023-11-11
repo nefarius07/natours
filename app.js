3989e31ff7441e7100563c60cdd73b93e5d9212f
Wojciech Jandura <wojtek.jandura@gmail.com>
2023-11-11 20:03:16 +0100

    Adding user routes

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const morgan = require('morgan');
 const app = express();
 
 //////////////////////////////////////////////////////
-// 1) MIDDLEWARE
+// 1) MIDDLEWARES
+
 app.use(morgan('dev'));
 app.use(express.json());
 
@@ -25,6 +26,7 @@ const tours = JSON.parse(
 
 //////////////////////////////////////////////////////
 // 2) ROUTE HANDLER
+
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -108,14 +110,44 @@ const deleteTour = (req, res) => {
   });
 };
 
-// app.get(`/api/v1/tours`, getAllTours);
-// app.post(`/api/v1/tours/`, createTour);
-// app.get(`/api/v1/tours/:id`, getTour);
-// app.patch(`/api/v1/tours/:id`, updateTour);
-// app.delete(`/api/v1/tours/:id`, deleteTour);
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
 
 //////////////////////////////////////////////////////
 // 3) ROUTES
+
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -123,6 +155,14 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 //////////////////////////////////////////////////////
 // 4) START SERVER
